afdb623dd9f9ba571ee6533c09278f0148658527
JuliaTru <90379069+JuliaTru@users.noreply.github.com>
2021-09-21 22:48:19 +0400

    Add files via upload

---
+++ src/main/java/com/game/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.game.repository;
+
+import com.game.entity.Player;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PlayerRepository extends JpaSpecificationExecutor<Player>,
+        PagingAndSortingRepository<Player, Long> {
+}
